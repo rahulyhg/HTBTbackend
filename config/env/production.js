75ecab358215a144aab52289fa690fff6ce4e7cc
Chintan Shah <chintan@wohlig.com>
2017-05-17 19:29:34 +0530

    done

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 80,
-  realHost: "http://tushar.io",
+  realHost: "http://htbt.wohlig.co.in",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com"]
 
   /***************************************************************************
