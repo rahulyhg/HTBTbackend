acc264e95bead6f25dbdcc51ee09dfb8deefe046
Chintan <chintan@wohlig.com>
2017-06-01 20:03:31 +0530

    dpone chanegs

--- frontend/js/controllers/controller.js
+++ frontend/js/controllers/controller.js
@@ -32,11 +32,11 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
   })
 
   //Example API Controller
-  .controller('DemoAPICtrl', function ($scope, TemplateService, apiService, NavigationService, $timeout) {
-    apiService.getDemo($scope.formData, function (data) {
-      console.log(data);
-    });
-  })
+  // .controller('DemoAPICtrl', function ($scope, TemplateService, apiService, NavigationService, $timeout) {
+  //   apiService.getDemo($scope.formData, function (data) {
+  //     console.log(data);
+  //   });
+  // })
 
   .controller('SignUpCtrl', function ($scope, TemplateService, $state, $stateParams, apiService, NavigationService, $timeout) {
     $scope.template = TemplateService.getHTML("content/signup.html");
@@ -71,26 +71,25 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
   .controller('ReviewCtrl', function ($scope, TemplateService, $stateParams, apiService, NavigationService, $uibModal, $timeout) {
     $scope.template = TemplateService.getHTML("content/review.html");
     TemplateService.title = "Review"; //This is the Title of the Website
-    apiService.getDemo($scope.formData, function (data) {
-      console.log(data);
-    });
+
     $scope.orderData = {};
+    $scope.amountToBePaid = 0;
     if ($stateParams.orderId) {
       console.log("orderId", $stateParams.orderId);
       var formData = {};
       formData._id = $stateParams.orderId;
       apiService.apiCall("Order/getOne", formData, function (data) {
         if (data.value === true) {
-          console.log("login", data.data);
+
           $scope.orderData = data.data;
-          var total = 0;
-          total = $scope.orderData.totalQuantity * $scope.orderData.product[0].finalPrice;
-          totalsave = $scope.orderData.totalQuantity * $scope.orderData.product[0].product.price;
-          $scope.diff = totalsave - total;
-          $scope.percent = total / totalsave * 100;
+          _.each($scope.orderData.product, function (n, key) {
+            $scope.amountToBePaid += parseFloat(n.product.price) * parseInt(n.productQuantity);
+          });
+
+
           $scope.options = {
             'key': 'rzp_test_BrwXxB7w8pKsfS',
-            'amount': parseInt($scope.orderData.totalAmount) * 100,
+            'amount': parseInt($scope.amountToBePaid) * 100,
             'name': $scope.orderData.customer.name,
             'description': 'Pay for Order ' + $scope.orderData.orderID,
             'image': '',
@@ -106,18 +105,9 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
               color: '#3399FF'
             }
           };
-          //  $.jStorage.set('user', data.data);
-          //  $.jStorage.set("accessToken", data.data.accessToken[0]);
+
         }
       });
-    }
-    $scope.calprice = function () {
-      var total = 0;
-      total = $scope.orderData.totalQuantity * $scope.orderData.product[0].finalPrice;
-      totalsave = $scope.orderData.totalQuantity * $scope.orderData.product[0].product.price;
-      var diff = totalsave - total;
-      $scope.percent = total / save * 100;
-      return diff;
     }
 
     $scope.pay = function () {
@@ -133,7 +123,7 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
       console.log("transaction", success);
       if (success.razorpay_payment_id) {
         $scope.orderData.razorpay_payment_id = success.razorpay_payment_id;
-         $scope.orderData.status = 'Confirmed';
+        $scope.orderData.status = 'Confirmed';
         apiService.apiCall("Order/orderConfirmationOrPay", $scope.orderData, function (data) {
           if (data.value === true) {
             console.log("payAndCapture");
@@ -366,15 +356,15 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
 
 
 
-    .controller('ThankYouCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
-        $scope.template = TemplateService.getHTML("content/thankyou.html");
-        TemplateService.title = "thankyou"; //This is the Title of the Website
-    })
+  .controller('ThankYouCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.getHTML("content/thankyou.html");
+    TemplateService.title = "thankyou"; //This is the Title of the Website
+  })
 
-    .controller('SorryCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
-        $scope.template = TemplateService.getHTML("content/Sorry.html");
-        TemplateService.title = "Sorry"; //This is the Title of the Website
-    })
+  .controller('SorryCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.getHTML("content/Sorry.html");
+    TemplateService.title = "Sorry"; //This is the Title of the Website
+  })
 
 
   .controller('headerctrl', function ($scope, TemplateService) {
@@ -390,4 +380,3 @@ myApp.controller('HomeCtrl', function ($scope, TemplateService, NavigationServic
     };
 
   });
-   
--- frontend/js/factories/api.js
+++ frontend/js/factories/api.js
@@ -1,16 +1,16 @@
 myApp.factory('apiService', function ($http, $q, $timeout) {
     // var adminurl = "http://192.168.1.121:1337/api/"; //server
-    
+
     return {
 
         // This is a demo Service for POST Method.
-        getDemo: function (formData, callback) {
-            $http({
-                url: adminurl + 'demo/demoService',
-                method: 'POST',
-                data: formData
-            }).success(callback);
-        },
+        // getDemo: function (formData, callback) {
+        //     $http({
+        //         url: adminurl + 'demo/demoService',
+        //         method: 'POST',
+        //         data: formData
+        //     }).success(callback);
+        // },
         // This is a demo Service for POST Method.
         apiCall: function (url, formData, callback) {
             $http.post(adminurl + url, formData).then(function (data) {
