14067c85d4145a5fd0e8a91c065a21caae816373
supriya-bhartiya <supriya.bhartiya7@gmail.com>
2017-06-01 18:55:38 +0530

    nice

--- frontend/js/factories/api.js
+++ frontend/js/factories/api.js
@@ -1,5 +1,5 @@
 myApp.factory('apiService', function ($http, $q, $timeout) {
-    var adminurl = "http://192.168.1.121:1337/api/"; //server
+    // var adminurl = "http://192.168.1.121:1337/api/"; //server
     
     return {
 
