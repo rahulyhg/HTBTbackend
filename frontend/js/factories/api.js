cf907c00c772cb9a37112d8693946a36df9daf87
Ganesh Kothavale <ganesh.wohlig@gmail.com>
2017-06-08 12:04:42 +0530

    done

--- frontend/js/factories/api.js
+++ frontend/js/factories/api.js
@@ -1,4 +1,4 @@
-myApp.factory('apiService', function ($http, $q, $timeout, $state) {
+myApp.factory('apiService', function($http, $q, $timeout, $state) {
 
     return {
 
@@ -11,11 +11,11 @@ myApp.factory('apiService', function ($http, $q, $timeout, $state) {
         //     }).success(callback);
         // },
         // This is a demo Service for POST Method.
-        apiCall: function (url, formData, callback) {
-            $http.post(adminurl + url, formData).then(function (data) {
+        apiCall: function(url, formData, callback) {
+            $http.post(adminurl + url, formData).then(function(data) {
                 data = data.data;
                 data.data.totalPrice = 0;
-                _.each(data.data.product, function (product) {
+                _.each(data.data.product, function(product) {
                     if (!product.finalPrice) {
                         product.finalPrice = parseFloat(product.product.price);
                     }
@@ -23,26 +23,29 @@ myApp.factory('apiService', function ($http, $q, $timeout, $state) {
                         data.data.type = "Subscription";
                     }
                     data.data.totalPrice += parseFloat(product.finalPrice) * parseInt(product.productQuantity);
+                    if (product.jarDeposit) {
+                        data.data.totalPrice += parseFloat(product.product.AmtDeposit) * parseInt(product.productQuantity);
+                    }
                 });
                 callback(data);
             });
         },
-        getPinDetail: function (formData, callback) {
-            $http.post(adminurl + "Pincode/getByPin", formData).then(function (data) {
+        getPinDetail: function(formData, callback) {
+            $http.post(adminurl + "Pincode/getByPin", formData).then(function(data) {
                 callback(data);
             });
         },
         getNextDate: function(stringArr) {
             var objs = [];
-            _.each(stringArr,function(day) {
-                var dateDay = moment(day,"dddd").toDate();
+            _.each(stringArr, function(day) {
+                var dateDay = moment(day, "dddd").toDate();
                 objs.push(dateDay);
             });
-            _.each(stringArr,function(day) {
-                var dateDay = moment(day,"dddd").add(1,"week").toDate();
+            _.each(stringArr, function(day) {
+                var dateDay = moment(day, "dddd").add(1, "week").toDate();
                 objs.push(dateDay);
             });
-            var remaining = _.filter(objs,function(dat) {
+            var remaining = _.filter(objs, function(dat) {
                 return !moment(dat).isSameOrBefore(moment(), 'day');
             });
             return remaining[0];
