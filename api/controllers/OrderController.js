fbf6631aa153ce42f1e84353d5e919fc8cd58a56
Chintan <chintan@wohlig.com>
2017-06-28 13:08:21 +0530

    done

--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -38,7 +38,27 @@ var controller = {
                 data: {
                     message: "Invalid Request"
                 }
-            })
+            });
+        }
+    },
+    orderConfirmationOrPayBackend: function (req, res) {
+        if (req.body) {
+            if (_.isEqual(JSON.parse(body).status, 'authorized')) {
+                req.body.paymentStatus = 'Paid';
+                req.body.status = 'Paid';
+                Order.orderConfirmationOrPay(req.body, res.callback);
+            } else if (_.isEqual(JSON.parse(body).status, 'failed')) {
+                req.body.paymentStatus = 'Payment Failed';
+                req.body.status = 'Payment Failed';
+                Order.orderConfirmationOrPay(req.body, res.callback);
+            }
+        } else {
+            res.json({
+                value: false,
+                data: {
+                    message: "Invalid Request"
+                }
+            });
         }
     },
     getOrderByUser: function (req, res) {
